25a7ba0c053b4581a7a5fa9b8cc87f5add6ea2b1
kairo_tech <44806673+gba100@users.noreply.github.com>
2023-02-24 02:50:09 +0200

    more fixes
    testing

--- project1.js
+++ project1.js
@@ -1,7 +1,7 @@
 function getWeatherData() {
     $("form").submit(function(event) {
         event.preventDefault();
-        var city = $("#city").val();
+        var city = $("#city").val().trim(); // remove whitespace from the beginning and end of the city string
         var API_KEY = "278942cc7350d2a53d3e933f351ba2bf";
         var API_URL =
             "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -10,31 +10,46 @@ function getWeatherData() {
             API_KEY;
         $.getJSON(API_URL, function(data) {
             var temp = Math.round(data.main.temp - 273.15);
-            $("#cityName").text("City: " + data.name);
-            $("#temp").text("Temperature: " + temp + "°C");
-
-            $("#humidity").text("Humidity: " + data.main.humidity + "%");
-            $("#windSpeed").text("Wind Speed: " + data.wind.speed + "m/s");
-            $("#desc").text("Description: " + data.weather[0].description);
-            $("#icon").html(
-                "<img src='http://openweathermap.org/img/wn/" +
-                data.weather[0].icon +
-                "@2x.png'>"
-            );
-            var sunriseTime = new Date(data.sys.sunrise * 1000);
-            var sunsetTime = new Date(data.sys.sunset * 1000);
-
-            $("#sunrise").text("Sunrise: " + sunriseTime.toLocaleTimeString([], {
-                hour: '2-digit',
-                minute: '2-digit'
-            }));
-            $("#sunset").text("Sunset: " + sunsetTime.toLocaleTimeString([], {
-                hour: '2-digit',
-                minute: '2-digit'
-            }));
-
+            var language = detectLanguage(city);
+            if (language === "hebrew") {
+                $("#cityName").text("עיר: " + data.name);
+                $("#temp").text("טמפרטורה: " + temp + "°C");
+                $("#humidity").text("לחות: " + data.main.humidity + "%");
+                $("#windSpeed").text("מהירות הרוח: " + data.wind.speed + "m/s");
+                $("#desc").text("תיאור: " + data.weather[0].description);
+                $("#sunrise").text("זריחה: " + new Date(data.sys.sunrise * 1000).toLocaleTimeString('he-IL', { hour: '2-digit', minute: '2-digit' }));
+                $("#sunset").text("שקיעה: " + new Date(data.sys.sunset * 1000).toLocaleTimeString('he-IL', { hour: '2-digit', minute: '2-digit' }));
+                $("#icon").html(
+                    "<img src='http://openweathermap.org/img/wn/" +
+                    data.weather[0].icon +
+                    "@2x.png'>"
+                );
+            } else {
+                $("#cityName").text("City: " + data.name);
+                $("#temp").text("Temperature: " + temp + "°C");
+                $("#humidity").text("Humidity: " + data.main.humidity + "%");
+                $("#windSpeed").text("Wind Speed: " + data.wind.speed + "m/s");
+                $("#desc").text("Description: " + data.weather[0].description);
+                $("#sunrise").text("Sunrise: " + new Date(data.sys.sunrise * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+                $("#sunset").text("Sunset: " + new Date(data.sys.sunset * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+                $("#icon").html(
+                    "<img src='http://openweathermap.org/img/wn/" +
+                    data.weather[0].icon +
+                    "@2x.png'>"
+                );
+            }
             // show weatherData div
             $("#weatherData").show();
         });
     });
+}
+
+function detectLanguage(text) {
+    var hebrewChars = /[\u0590-\u05FF]/;
+    for (var i = 0; i < text.length; i++) {
+        if (hebrewChars.test(text.charAt(i))) {
+            return "hebrew";
+        }
+    }
+    return "english";
 }
