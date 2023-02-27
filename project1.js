745f1007243b76e412a2c70b86feb3381f9c0a2a
kairo_tech <44806673+gba100@users.noreply.github.com>
2023-02-27 15:35:32 +0200

    beta 1.2
    *weather app got forecast for the next five days.
    * responsiveness fixes
    *bug fixes

--- project1.js
+++ project1.js
@@ -1,3 +1,50 @@
+const API_KEY = "278942cc7350d2a53d3e933f351ba2bf";
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=" + API_KEY;
+
+const form = document.querySelector('.form');
+const weatherContainer = document.querySelector('.weather');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    let city = document.querySelector('#city').value.trim(); // remove leading/trailing spaces
+    const hebrewRegex = /[\u0590-\u05FF]/; // regex to check for Hebrew characters
+
+    const language = hebrewRegex.test(city) ? 'he' : 'en'; // determine language based on input
+
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric&lang=${language}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const forecastData = data.list.filter((item, index) => index % 8 === 0);
+
+            const forecastHtml = forecastData.map(day => `
+        <div class="day">
+          <div>${formatDate(day.dt, language)}</div>
+          <div><img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"alt="${day.weather[0].description}" style="width: 80px; height: 80px;" /></div>
+          <div class="temp">${day.main.temp.toFixed(0)}°C</div>
+          <div class="description">${day.weather[0].description}</div>
+        </div>
+      `).join('');
+
+            document.querySelector('.forecast').innerHTML = forecastHtml;
+
+            weatherContainer.style.display = 'block';
+        })
+        .catch(error => console.error(error));
+});
+
+function formatDate(timestamp, lang) {
+    const date = new Date(timestamp * 1000);
+    const dayOfWeek = new Intl.DateTimeFormat(lang, { weekday: 'short' }).format(date);
+    const month = new Intl.DateTimeFormat(lang, { month: 'short' }).format(date);
+    const dayOfMonth = date.getDate();
+    return `${dayOfWeek}, ${dayOfMonth} ${month}`;
+}
+
+
+
 function getWeatherData() {
     $("form").submit(function(event) {
         event.preventDefault();
